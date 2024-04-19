56030f5421f9ac3efc5a4f671607d7123c928835
JS7542 <dbs001228@gmail.com>
2024-04-19 21:18:27 +0900

    2024-04-19 - BOJ - 10170.java

---
+++ BOJ/Bronze 5/BOJ - 10170.java
@@ -0,0 +1,9 @@
+class Main {
+    /*
+     * 문제에서 제시한 팀 이름과 승패 기록 표를 그대로 출력한다.
+     * 공백과 줄바꿈을 원본 형식에 맞춘다.
+     */
+    public static void main(String[] args){
+        System.out.print("NFC West       W   L  T\n-----------------------\nSeattle        13  3  0\nSan Francisco  12  4  0\nArizona        10  6  0\nSt. Louis      7   9  0\n\nNFC North      W   L  T\n-----------------------\nGreen Bay      8   7  1\nChicago        8   8  0\nDetroit        7   9  0\nMinnesota      5  10  1");
+    }
+}
