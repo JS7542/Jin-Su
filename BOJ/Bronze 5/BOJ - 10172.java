c3526d3b118e830780708be489efd33773495634
JS7542 <dbs001228@gmail.com>
2024-04-19 22:08:44 +0900

    2024-04-19 - BOJ - 10172.java

---
+++ BOJ/Bronze 5/BOJ - 10172.java
@@ -0,0 +1,13 @@
+class Main {
+    /*
+     * 역슬래시와 따옴표가 포함된 개 모양을 출력한다.
+     * 특수문자는 이스케이프 처리해 문제의 출력과 동일하게 만든다.
+     */
+    public static void main(String[] args) {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
