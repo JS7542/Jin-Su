cc9b945c2933be3c4234922179712ae768254083
JS7542 <dbs001228@gmail.com>
2024-04-25 21:08:25 +0900

    2024-04-25 - BOJ - 5337.java

---
+++ BOJ/Bronze 5/BOJ - 5337.java
@@ -0,0 +1,9 @@
+class Main {
+    /*
+     * 문제에서 요구한 고정된 ASCII 문양을 출력한다.
+     * 역슬래시는 자바 문자열에서 이스케이프 처리한다.
+     */
+    public static void main(String[] args){
+        System.out.print(".  .   .\n|  | _ | _. _ ._ _  _\n|/\\|(/.|(_.(_)[ | )(/.");
+    }
+}
