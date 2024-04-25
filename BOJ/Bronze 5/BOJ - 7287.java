292500e70b33a8a3f6c5f335f081fcd86668afb7
JS7542 <dbs001228@gmail.com>
2024-04-25 23:06:55 +0900

    2024-04-25 - BOJ - 7287.java

---
+++ BOJ/Bronze 5/BOJ - 7287.java
@@ -0,0 +1,9 @@
+class Main {
+    /*
+     * 본인의 맞은 문제 수와 아이디를 출력하는 출력 전용 문제다.
+     * 제출 환경에 맞게 두 값을 고정 문자열로 작성한다.
+     */
+    public static void main(String[] args){
+        System.out.print("0\nyoonjinsu");
+    }
+}
