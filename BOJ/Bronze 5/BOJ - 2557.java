92936ff4aca517cf0cfb780044125bc0795ead96
JS7542 <dbs001228@gmail.com>
2024-04-25 23:23:30 +0900

    2024-04-25 - BOJ - 2557.java

---
+++ BOJ/Bronze 5/BOJ - 2557.java
@@ -0,0 +1,9 @@
+class Main {
+    /*
+     * 별도의 입력 없이 지정된 문자열을 출력한다.
+     * 출력 형식을 그대로 지키는 가장 기본적인 입출력 문제다.
+     */
+    public static void main(String[] args) {
+        System.out.print("Hello World!");
+    }
+}
