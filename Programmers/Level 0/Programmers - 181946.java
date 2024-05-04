2b1f53be6cdefb28e86b19908300529c7d9c2a96
JS7542 <dbs001228@gmail.com>
2024-05-04 22:21:39 +0900

    2024-05-04 - Programmers - 181946.java

---
+++ Programmers/Level 0/Programmers - 181946.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    /*
+     * 공백으로 구분된 두 문자열을 입력받는다.
+     * 별도의 공백 없이 두 문자열을 이어 붙여 출력한다.
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String first = sc.next();
+        String second = sc.next();
+        System.out.print(first + second);
+    }
+}
