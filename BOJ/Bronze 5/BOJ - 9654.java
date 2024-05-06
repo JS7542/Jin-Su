aa64ed371943e291b57a1126e2eaa963a1182d85
JS7542 <dbs001228@gmail.com>
2024-05-06 22:25:34 +0900

    2024-05-06 - BOJ - 9654.java

---
+++ BOJ/Bronze 5/BOJ - 9654.java
@@ -0,0 +1,9 @@
+class Main {
+    /*
+     * 문제에서 지정한 표의 헤더와 세 행을 그대로 출력한다.
+     * 열 사이 공백 수를 정확히 유지한다.
+     */
+    public static void main(String[] args){
+        System.out.print("SHIP NAME      CLASS          DEPLOYMENT IN SERVICE\nN2 Bomber      Heavy Fighter  Limited    21\nJ-Type 327     Light Combat   Unlimited  1\nNX Cruiser     Medium Fighter Limited    18\nN1 Starfighter Medium Fighter Unlimited  25\nRoyal Cruiser  Light Combat   Limited    4");
+    }
+}
