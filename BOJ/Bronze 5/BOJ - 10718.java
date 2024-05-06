92c0be0261ad3971b5786352ee2c190793c047bb
JS7542 <dbs001228@gmail.com>
2024-05-06 21:55:27 +0900

    2024-05-06 - BOJ - 10718.java

---
+++ BOJ/Bronze 5/BOJ - 10718.java
@@ -0,0 +1,9 @@
+class Main {
+    /*
+     * 지정된 문장을 정확히 두 번 출력한다.
+     * 각 문장은 줄바꿈으로 구분한다.
+     */
+    public static void main(String[] args){
+        System.out.print("강한친구 대한육군\n강한친구 대한육군");
+    }
+}
