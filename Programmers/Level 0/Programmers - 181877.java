43148b49cde72f12d5c7be7b2d88d69da8078179
JS7542 <dbs001228@gmail.com>
2024-05-17 21:54:36 +0900

    2024-05-17 - Programmers - 181877.java

---
+++ Programmers/Level 0/Programmers - 181877.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * 문자열의 모든 영문 소문자를 대문자로 변환한다.
+     * 변환된 문자열을 그대로 반환한다.
+     */
+    public String solution(String myString) {
+        return myString.toUpperCase();
+    }
+}
