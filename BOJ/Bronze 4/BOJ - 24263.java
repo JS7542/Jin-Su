04bf579cc4666d1080a8cd0479d4fb2d4e520764
JS7542 <dbs001228@gmail.com>
2024-05-18 23:18:07 +0900

    2024-05-18 - BOJ - 24263.java

---
+++ BOJ/Bronze 4/BOJ - 24263.java
@@ -0,0 +1,13 @@
+import java.io.*;
+
+class Main {
+    /*
+     * 반복문이 N번 수행되므로 실행 횟수는 N이다.
+     * 최고차항의 차수는 1이므로 N과 1을 출력한다.
+     */
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(br.readLine());
+        System.out.print(1);
+    }
+}
