d83bb3d5c70839500043727a9e93e9237ad447a2
JS7542 <dbs001228@gmail.com>
2024-05-29 21:19:25 +0900

    2024-05-29 - Programmers - 181951.java

---
+++ Programmers/Level 0/Programmers - 181951.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    /*
+     * 두 정수 a와 b를 입력받는다.
+     * 각 값을 문제에서 지정한 이름과 줄바꿈 형식으로 출력한다.
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println("a = " + a);
+        System.out.print("b = " + b);
+    }
+}
