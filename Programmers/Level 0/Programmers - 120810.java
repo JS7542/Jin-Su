79559ca1e0ff02c74778be6c327e7be44f041d5d
JS7542 <dbs001228@gmail.com>
2024-05-29 21:30:05 +0900

    2024-05-29 - Programmers - 120810.java

---
+++ Programmers/Level 0/Programmers - 120810.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * num1을 num2로 나눈 나머지를 계산한다.
+     * 나머지 연산자 결과를 그대로 반환한다.
+     */
+    public int solution(int num1, int num2) {
+        return num1 % num2;
+    }
+}
