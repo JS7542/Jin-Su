bdb3336c4bd926beec427d855d04301efc6a11f1
JS7542 <dbs001228@gmail.com>
2024-06-14 21:19:10 +0900

    2024-06-14 - BOJ - 10171.java

---
+++ BOJ/Bronze 5/BOJ - 10171.java
@@ -0,0 +1,12 @@
+class Main {
+    /*
+     * 역슬래시와 따옴표가 포함된 고양이 모양을 출력한다.
+     * 자바 문자열의 이스케이프 문자를 정확하게 사용한다.
+     */
+    public static void main(String[] args) {
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+    }
+}
