5e7977aa332143bffaf7ecb87257d2d2b627c92a
JS7542 <dbs001228@gmail.com>
2024-06-16 21:29:23 +0900

    2024-06-16 - BOJ - 24262.java

---
+++ BOJ/Bronze 5/BOJ - 24262.java
@@ -0,0 +1,10 @@
+class Main {
+    /*
+     * 주어진 알고리즘은 입력 크기와 관계없이 한 번만 수행된다.
+     * 수행 횟수 상수 1과 최고차항 차수 0을 출력한다.
+     */
+    public static void main(String[] args) {
+        System.out.println(1);
+        System.out.print(0);
+    }
+}
