a1d9daa2c5e0267fb54f922c37c9440bf4be06b0
JS7542 <dbs001228@gmail.com>
2024-06-16 21:50:22 +0900

    2024-06-16 - BOJ - 10926.java

---
+++ BOJ/Bronze 5/BOJ - 10926.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+    /*
+     * 입력받은 아이디 뒤에 문제에서 지정한 놀람 문자열을 붙인다.
+     * 완성된 문자열을 한 줄로 출력한다.
+     */
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.print(br.readLine() + "??!");
+    }
+}
