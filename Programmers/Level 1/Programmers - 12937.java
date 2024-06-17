fc337c5176cb1b48f04a22b1283612973877249d
JS7542 <dbs001228@gmail.com>
2024-06-17 22:23:30 +0900

    2024-06-17 - Programmers - 12937.java

---
+++ Programmers/Level 1/Programmers - 12937.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * 정수를 2로 나눈 나머지를 확인한다.
+     * 나머지가 0이면 Even, 아니면 Odd를 반환한다.
+     */
+    public String solution(int num) {
+        return num % 2 == 0 ? "Even" : "Odd";
+    }
+}
