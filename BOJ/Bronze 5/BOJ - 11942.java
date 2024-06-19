e2a4b81e27670e1037f3117eb6c1703af9a63a9e
JS7542 <dbs001228@gmail.com>
2024-06-19 23:23:43 +0900

    2024-06-19 - BOJ - 11942.java

---
+++ BOJ/Bronze 5/BOJ - 11942.java
@@ -0,0 +1,9 @@
+class Main {
+    /*
+     * 별도의 입력 없이 문제에서 요구한 고정 문자열을 출력한다.
+     * 대소문자와 띄어쓰기를 정확하게 유지한다.
+     */
+    public static void main(String[] args) {
+        System.out.print("고려대학교");
+    }
+}
