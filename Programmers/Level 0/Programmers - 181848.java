4ad5be38895b0840decba63a38dfe6db70589587
JS7542 <dbs001228@gmail.com>
2024-07-22 22:32:40 +0900

    2024-07-22 - Programmers - 181848.java

---
+++ Programmers/Level 0/Programmers - 181848.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * 숫자로 이루어진 문자열을 정수형으로 변환한다.
+     * Integer.parseInt의 결과를 반환한다.
+     */
+    public int solution(String n_str) {
+        return Integer.parseInt(n_str);
+    }
+}
