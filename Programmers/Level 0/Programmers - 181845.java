5673bb2cfcce1e62660579c098de7536f7025939
JS7542 <dbs001228@gmail.com>
2024-08-04 21:31:29 +0900

    2024-08-04 - Programmers - 181845.java

---
+++ Programmers/Level 0/Programmers - 181845.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * 정수 n을 문자열 표현으로 변환한다.
+     * String.valueOf의 결과를 그대로 반환한다.
+     */
+    public String solution(int n) {
+        return String.valueOf(n);
+    }
+}
