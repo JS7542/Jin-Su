9ad44ae21d5f1d8ba1a341e4b803a0875ad9928b
JS7542 <dbs001228@gmail.com>
2024-08-05 23:02:42 +0900

    2024-08-05 - Programmers - 120820.java

---
+++ Programmers/Level 0/Programmers - 120820.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * 기준 연도 2022에서 나이를 이용해 출생 연도를 계산한다.
+     * 태어난 해를 한 살로 계산하므로 2023-age를 반환한다.
+     */
+    public int solution(int age) {
+        return 2023 - age;
+    }
+}
