1a0a8bf7b88bdf0e950b298bc6a3738352db634f
JS7542 <dbs001228@gmail.com>
2024-08-12 21:13:49 +0900

    2024-08-12 - Programmers - 181952.java

---
+++ Programmers/Level 0/Programmers - 181952.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+class Solution {
+    /*
+     * 문자열 한 개를 표준 입력으로 읽는다.
+     * 입력받은 문자열을 변경 없이 그대로 출력한다.
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(sc.next());
+    }
+}
