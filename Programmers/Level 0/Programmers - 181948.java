c5ac77c446bb0ca90f6c81475e60b38098eea357
JS7542 <dbs001228@gmail.com>
2024-08-12 22:33:25 +0900

    2024-08-12 - Programmers - 181948.java

---
+++ Programmers/Level 0/Programmers - 181948.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * 문제에서 지정한 특수문자 문자열을 그대로 출력한다.
+     * 역슬래시와 큰따옴표는 자바 이스케이프 문법으로 표현한다.
+     */
+    public static void main(String[] args) {
+        System.out.print("!@#$%^&*(\\'\"<>?:;");
+    }
+}
