b6795534d50a29ee0c25bcc77de37872b7cc9a9d
JS7542 <dbs001228@gmail.com>
2024-08-13 22:26:48 +0900

    2024-08-13 - Programmers - 181863.java

---
+++ Programmers/Level 0/Programmers - 181863.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * 문자열에 포함된 모든 m 문자를 rn 문자열로 치환한다.
+     * 치환된 결과 문자열을 반환한다.
+     */
+    public String solution(String rny_string) {
+        return rny_string.replace("m", "rn");
+    }
+}
