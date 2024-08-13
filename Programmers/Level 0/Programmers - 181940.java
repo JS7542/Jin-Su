c8012ba80c3e768d6de44dc8b02894fea3c26cdd
JS7542 <dbs001228@gmail.com>
2024-08-13 23:26:56 +0900

    2024-08-13 - Programmers - 181940.java

---
+++ Programmers/Level 0/Programmers - 181940.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * 입력 문자열을 k번 반복한다.
+     * String.repeat으로 완성된 문자열을 반환한다.
+     */
+    public String solution(String my_string, int k) {
+        return my_string.repeat(k);
+    }
+}
