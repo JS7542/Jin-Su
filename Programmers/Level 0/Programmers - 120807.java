55865ca528100787bd2f61ddacd0a896d85f51e7
JS7542 <dbs001228@gmail.com>
2024-08-27 21:34:11 +0900

    2024-08-27 - Programmers - 120807.java

---
+++ Programmers/Level 0/Programmers - 120807.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * 두 정수 num1과 num2가 같은지 비교한다.
+     * 같으면 1, 다르면 -1을 반환한다.
+     */
+    public int solution(int num1, int num2) {
+        return num1 == num2 ? 1 : -1;
+    }
+}
