3a5c3479c267d272ae2e4fc5e34d16e434222166
JS7542 <dbs001228@gmail.com>
2024-09-05 21:36:53 +0900

    2024-09-05 - Programmers - 181876.java

---
+++ Programmers/Level 0/Programmers - 181876.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * 문자열의 모든 영문 대문자를 소문자로 변환한다.
+     * 변환된 문자열을 그대로 반환한다.
+     */
+    public String solution(String myString) {
+        return myString.toLowerCase();
+    }
+}
