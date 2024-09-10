352819852baeca163ffac2e681d2dc234a127858
JS7542 <dbs001228@gmail.com>
2024-09-10 23:08:24 +0900

    2024-09-10 - Programmers - 181947.java

---
+++ Programmers/Level 0/Programmers - 181947.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Solution {
+    /*
+     * 두 정수 a와 b를 입력받아 합을 계산한다.
+     * a + b = 결과 형식으로 덧셈식을 출력한다.
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.print(a + " + " + b + " = " + (a + b));
+    }
+}
