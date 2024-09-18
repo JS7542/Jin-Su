c60b96c4fa3c4e7e5df9106cc5a5d0f0f8c0210c
JS7542 <dbs001228@gmail.com>
2024-09-18 21:45:38 +0900

    2024-09-18 - Programmers - 120898.java

---
+++ Programmers/Level 0/Programmers - 120898.java
@@ -0,0 +1,7 @@
+class Solution {
+    /*
+     * 한 글자를 가로 2cm 크기로 적으므로 전체 길이는 글자 수의 두 배다.
+     * 문자열 길이에 2를 곱해 반환한다.
+     */
+    public int solution(String message){return message.length()*2;}
+}
