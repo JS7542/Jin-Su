94cb08fff6ee06af768326e7792efa39382b2459
JS7542 <dbs001228@gmail.com>
2024-09-21 23:05:08 +0900

    2024-09-21 - Programmers - 120805.java

---
+++ Programmers/Level 0/Programmers - 120805.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * num1을 num2로 나눈 정수 몫을 계산한다.
+     * 자바 정수 나눗셈 결과를 그대로 반환한다.
+     */
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
