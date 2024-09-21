50198b6627271429f293f7139ce25a9fca83639c
JS7542 <dbs001228@gmail.com>
2024-09-21 21:03:41 +0900

    2024-09-21 - Programmers - 181941.java

---
+++ Programmers/Level 0/Programmers - 181941.java
@@ -0,0 +1,9 @@
+class Solution {
+    /*
+     * 문자열 배열의 각 원소를 입력 순서대로 이어 붙인다.
+     * 완성된 하나의 문자열을 반환한다.
+     */
+    public String solution(String[] arr) {
+        return String.join("", arr);
+    }
+}
