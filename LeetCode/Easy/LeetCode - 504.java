e8b73253801bb75c4757484a7a2020e9860fc9a1
JS7542 <dbs001228@gmail.com>
2025-03-11 21:32:48 +0900

    2025-03-11 - LeetCode - 504.java

---
+++ LeetCode/Easy/LeetCode - 504.java
@@ -0,0 +1,7 @@
+class Solution {
+    /*
+     * 정수를 7진수 문자열로 변환한다.
+     * 자바의 Integer.toString에 기수 7을 지정해 반환한다.
+     */
+    public String convertToBase7(int num){return Integer.toString(num,7);}
+}
