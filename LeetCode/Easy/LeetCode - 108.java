3014e6b0b9af09ab4917bd72b46b9fbe4e078c07
JS7542 <dbs001228@gmail.com>
2025-06-09 23:37:32 +0900

    2025-06-09 - LeetCode - 108.java

---
+++ LeetCode/Easy/LeetCode - 108.java
@@ -0,0 +1,27 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+}
+
+class Solution {
+    /*
+     * 정렬 배열의 가운데 값을 현재 서브트리의 루트로 선택한다.
+     * 왼쪽과 오른쪽 구간을 재귀적으로 처리해 균형 BST를 만든다.
+     */
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return build(nums, 0, nums.length - 1);
+    }
+
+    private TreeNode build(int[] nums, int left, int right) {
+        if (left > right) return null;
+
+        int mid = left + (right - left) / 2;
+        TreeNode root = new TreeNode(nums[mid]);
+        root.left = build(nums, left, mid - 1);
+        root.right = build(nums, mid + 1, right);
+        return root;
+    }
+}
